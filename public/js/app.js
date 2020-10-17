83bfc429b4a5a8eae6c6f833c68f4ad86a78e7ea
nikolovskipetar <dw-petar@hotmail.com>
2020-10-17 16:08:29 +0200

    updated heroku

--- public/js/app.js
+++ public/js/app.js
@@ -6,7 +6,7 @@ weatherData.addEventListener('submit', (e) => {
   e.preventDefault()
   const location = document.getElementById('input').value
 
-  fetch(window.location.href + `weather?address=${location}`).then((res) => {
+  fetch(`/weather?address=${location}`).then((res) => {
     res.json().then((data) => {
       if(data.error) {
         console.log(data)
--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const geocode = require('./utils/geocode')
 const forecast = require('./utils/weather')
 
 const app = express()
+const port = process.env.PORT || 3000
 
 //Define paths for Express config
 const publicDir = path.join(__dirname, '../public')
@@ -75,4 +76,4 @@ app.get('*', (req, res) => {
 })
 
 
-app.listen(3000)
+app.listen(port)
